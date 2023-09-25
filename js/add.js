4c7dcbeb1d2bf6cd097a728c76acb1f887dc7823
SMRebelo <137157267+SMRebelo@users.noreply.github.com>
2023-09-25 02:21:55 +0100

    finish with the js function and functionality . css almost done. need to review

--- js/add.js
+++ js/add.js
@@ -29,41 +29,72 @@ function displayItems() {
         </div>
       </div>`;
   }
-  //? div class="item" this works as a general div for the text area i will display 
+  //? div class="item" this works as a general div for the text area i will display
   //?the disabled proprety doesnt let the user interact with the text area. I implement the itemsAray[i] to display the iterations of the for loop. i can use itemsArray in here because i save it as a constante.
   //? ICON check from "font awesome" i also add a class for delete button because this will erase all task checked
   //?CON edit from "font awesome" i also add a class for edit button because this will edit the input
 
   document.querySelector(".to-do-list").innerHTML = items; //? here im acessing the empty div #to-do-list i puting there the inf o want to display. we have to call it by ".to-do-list" because we are calling a class with querySelector.
-    activateDeleteListeners(); //? im calling all this functions here because i want then to run as soon as i run the "displayItems" function.
-  activateEditListeners();
+  activateDeleteListeners(); //? im calling all this functions here because i want then to run as soon as i run the "displayItems" function.
+  activateEditListeners();  
   activateSaveListeners();
   activateCancelListeners();
 }
 function activateDeleteListeners() {
-    let deleteBtn = document.querySelectorAll(".deleteBtn"); //? here im creating a delete opcion inside the function
-    deleteBtn.forEach((db, i) =>{ //? here im giving the delete button "db" the ability to delete several input depending on the "i" that is selected.
-        db.addEventListener("click", () => { deleteItem(i)}) //? here im adding an click event to the "db" button so it deletes the input selected "i". 
-    })
+  let deleteBtn = document.querySelectorAll(".deleteBtn"); //? here im creating a delete opcion inside the function
+  deleteBtn.forEach((db, i) => {
+    //? here im giving the delete button "db" the ability to delete several input depending on the "i" that is selected.
+    db.addEventListener("click", () => {
+      deleteItem(i);
+    }); //? here im adding an click event to the "db" button so it deletes the input selected "i".
+  });
 }
 function activateEditListeners() {
-    const editBtn = document.querySelectorAll(".editBtn") //? getting access to the edit button 
-    const updateController = document.querySelectorAll(".update-controller");//? getting access to the update button
-    const inputs = document.querySelectorAll(".input-controller textarea"); //? getting access to the text area for the edit function.
-    editBtn.forEach((eb, i) =>{
-        eb.addEventListener("click", () =>{ //? same logic as the delete button "db".
-            updateController[i].style.display = "block"; //? this gets the class created in the styles.css and change it to visible.
-            inputs[i].disable = false; //? and this one disables the property "disable" so the user can write text in the text area
+    
+  const editBtn = document.querySelectorAll(".editBtn"); //? getting access to the edit button
+ 
+  const updateController = document.querySelectorAll(".update-controller"); //? getting access to the update button
+  const inputs = document.querySelectorAll(".input-controller textarea"); //? getting access to the text area for the edit function.
+  editBtn.forEach((eb, i) => {
+    eb.addEventListener("click", () => {
+      //? same logic as the delete button "db".
+      updateController[i].style.display = "block"; //? this gets the class created in the styles.css and change it to visible.     
+      inputs[i].disabled = false; //? and this one disables the property "disable" so the user can write text in the text area
+     
+    });
+  });
+}
+function activateSaveListeners() {
+    const saveBtn = document.querySelectorAll(".saveBtn")
+    const inputs = document.querySelectorAll(".input-controller textarea")
+    saveBtn.forEach((sb, i) =>  {
+        sb.addEventListener("click", () => {
+            updateItem(inputs[i].value, i)//? this is to passe the inf the provided by the forEach. the updateItems functian will you JSON to save it in local storage.
+        })
+    })
+}
+function updateItem(text, i){
+    itemsArray[i] = text //? it gets the current index the user is editing and saves it as an argument in "text".
+    localStorage.setItem("items", JSON.stringify(itemsArray))
+    location.reload();
+}
+function activateCancelListeners(){
+    const cancelBtn = document.querySelectorAll(".cancelBtn")
+    const updateController =  document.querySelectorAll(".update-controller")
+    const inputs = document.querySelectorAll(".input-controller textarea")
+    cancelBtn.forEach((cb, i) => {
+        cb.addEventListener("click", () => {
+            updateController[i].style.display = "none";
+            inputs[i].disabled = true;
         })
     })
 }
 
 function deleteItem(i) {
-    itemsArray.splice(i,1); //? the slice function deletes elements. the "1" parameter its because i only want to delete one element.
-    localStorage.setItem("items", JSON.stringify(itemsArray)); //? here im updating the local storage with the deleted item. Again transforming the input in a sting and adding it to the  object itemsArray.
-    location.reload(); // this refresh the page
+  itemsArray.splice(i, 1); //? the slice function deletes elements. the "1" parameter its because i only want to delete one element.
+  localStorage.setItem("items", JSON.stringify(itemsArray)); //? here im updating the local storage with the deleted item. Again transforming the input in a sting and adding it to the  object itemsArray.
+  location.reload(); // this refresh the page
 }
-
 
 function createItem(item) {
   //? This function will add the items we read on the "click button" #enter function and save it on local storage.
@@ -73,5 +104,5 @@ function createItem(item) {
 }
 
 window.onload = function () {
-    displayItems();
+  displayItems();
 };
